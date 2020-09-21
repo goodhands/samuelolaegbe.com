dba31cdde87aa18801131fdb6a1c0d28f3cdfc13
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-21 14:40:00 +0100

    experience: add stylishcare

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -34,21 +34,21 @@
                         <!-- TODO:An API that serves a single source of truth for when i update my resume  -->
                         
                         <!-- HNG -->
-                        <div class="font-body resume-item flex flex-row justify-between">
+                        <div class="font-body resume-item flex mt-10 flex-row justify-between">
                             <div class="resume-body w-9/12">
-                                <h5 class="mb-3">
+                                <h5 class="mb-3 text-xl">
                                     <span class="font-bold">HNG Bootcamp</span>
                                     -
                                     Back End Developer 
                                 </h5>
-                                <p class="mb-3">
+                                <p class="mb-3 text-lg text-secondary">
                                     As tech lead, I led a team of 20+ developers and 
                                     guided the architecture and scope of the the Expense NG project 
                                     by providing a walk-through of the government's Open Treasury 
                                     (opentreasury.gov.ng) website which was crawled to fetch the 
                                     expense sheets for each year.
                                 </p>
-                                <ul class="list-disc">
+                                <ul class="list-disc text-lg text-secondary">
                                     <li>Introduced and enforced consistent coding conventions and standards across the application.</li>
                                     <li>Allowed visitors to make comments and interact with each other with a comments section which is available on each company/ministry's page, the comments component was built by me with Vue.js, Pusher and Laravel Echo</li>
                                 </ul>
@@ -60,23 +60,32 @@
                         <!-- End HNG -->
 
                         <!-- Stylishcare -->
-                        <div class="font-body resume-item flex flex-row justify-between">
+                        <div class="font-body resume-item flex mt-10 flex-row justify-between">
                             <div class="resume-body w-9/12">
-                                <h5 class="mb-3">
+                                <h5 class="mb-3 text-xl">
                                     <span class="font-bold">Stylishcare</span>
                                     -
                                     Full Stack Engineer (Contractor)
                                 </h5>
-                                <p class="mb-3">
-                                    As tech lead, I led a team of 20+ developers and 
-                                    guided the architecture and scope of the the Expense NG project 
-                                    by providing a walk-through of the government's Open Treasury 
-                                    (opentreasury.gov.ng) website which was crawled to fetch the 
-                                    expense sheets for each year.
+                                <p class="mb-3 text-lg text-secondary">
+                                    Led the development of a feature rich Warehouse management software for the company built as an efficient single page application using Laravel & Vue.js to replace the previous software which had a lot of restrictions and didn't fit their business plan.
                                 </p>
-                                <ul class="list-disc">
-                                    <li>Introduced and enforced consistent coding conventions and standards across the application.</li>
-                                    <li>Allowed visitors to make comments and interact with each other with a comments section which is available on each company/ministry's page, the comments component was built by me with Vue.js, Pusher and Laravel Echo</li>
+                                <ul class="list-disc text-lg text-secondary">
+                                    <li> 
+                                        By implementing a simple and accessible user interface for the packer to 
+                                        see all that is going on at once and also give proper feedback as each 
+                                        product is scanned, employees have recorded a decrease in time taken to 
+                                        package an order and ship by 50%.
+                                    </li>
+                                    <li>
+                                        Client was satisfied with the solution which aligns properly with their 
+                                        business goals and allows them to in turn satisfy customers with accurate 
+                                        & just-in-time shipping.
+                                    </li>
+                                    <li> 
+                                        Implemented audio and visual feedback across the admin dashboard to aid the 
+                                        packers who may not be paying attention while scanning orders.
+                                    </li>
                                 </ul>
                             </div>
                             <div id="timeline">
