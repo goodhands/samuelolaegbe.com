7b4a14f47508ecc6126fb88a29d5d824272610cf
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-21 15:24:55 +0100

    add stylishcare to YAML

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -30,69 +30,29 @@
                     </p>
 
                     <section class="mt-10">
-                        <h4 class="text-2xl mb-5">Work Experience</h4>
-                        <!-- TODO:An API that serves a single source of truth for when i update my resume  -->
-                        
-                        <!-- HNG -->
-                        <div class="font-body resume-item flex mt-10 flex-row justify-between">
+                        <h4 class="text-2xl mb-5">Work Experience</h4>                        
+                        <!-- Experience -->
+                        <div class="font-body resume-item flex mt-10 flex-row justify-between" v-for="work in experience" :key="work.company">
                             <div class="resume-body w-9/12">
                                 <h5 class="mb-3 text-xl">
-                                    <span class="font-bold">HNG Bootcamp</span>
+                                    <span class="font-bold">{{ work.company }}</span>
                                     -
-                                    Back End Developer 
+                                    {{ work.role }} 
                                 </h5>
                                 <p class="mb-3 text-lg text-secondary">
-                                    As tech lead, I led a team of 20+ developers and 
-                                    guided the architecture and scope of the the Expense NG project 
-                                    by providing a walk-through of the government's Open Treasury 
-                                    (opentreasury.gov.ng) website which was crawled to fetch the 
-                                    expense sheets for each year.
+                                    {{ work.summary }}
                                 </p>
-                                <ul class="list-disc text-lg text-secondary">
-                                    <li>Introduced and enforced consistent coding conventions and standards across the application.</li>
-                                    <li>Allowed visitors to make comments and interact with each other with a comments section which is available on each company/ministry's page, the comments component was built by me with Vue.js, Pusher and Laravel Echo</li>
-                                </ul>
-                            </div>
-                            <div id="timeline">
-                                June 2020 - August 2020 
-                            </div>
-                        </div>
-                        <!-- End HNG -->
-
-                        <!-- Stylishcare -->
-                        <div class="font-body resume-item flex mt-10 flex-row justify-between">
-                            <div class="resume-body w-9/12">
-                                <h5 class="mb-3 text-xl">
-                                    <span class="font-bold">Stylishcare</span>
-                                    -
-                                    Full Stack Engineer (Contractor)
-                                </h5>
-                                <p class="mb-3 text-lg text-secondary">
-                                    Led the development of a feature rich Warehouse management software for the company built as an efficient single page application using Laravel & Vue.js to replace the previous software which had a lot of restrictions and didn't fit their business plan.
-                                </p>
-                                <ul class="list-disc text-lg text-secondary">
-                                    <li> 
-                                        By implementing a simple and accessible user interface for the packer to 
-                                        see all that is going on at once and also give proper feedback as each 
-                                        product is scanned, employees have recorded a decrease in time taken to 
-                                        package an order and ship by 50%.
-                                    </li>
+                                <ul class="list-disc text-lg text-secondary" v-for="(achievements, index) in work.achievements" :key="index">
                                     <li>
-                                        Client was satisfied with the solution which aligns properly with their 
-                                        business goals and allows them to in turn satisfy customers with accurate 
-                                        & just-in-time shipping.
-                                    </li>
-                                    <li> 
-                                        Implemented audio and visual feedback across the admin dashboard to aid the 
-                                        packers who may not be paying attention while scanning orders.
+                                        {{ achievements }}
                                     </li>
                                 </ul>
                             </div>
                             <div id="timeline">
-                                May 2020 - July 2020 
+                                {{ work.start }} - {{ work.end }}
                             </div>
                         </div>
-                        <!-- End Stylishcare -->
+                        <!-- End Experience -->
                     </section>
                 </div>
             </section>
