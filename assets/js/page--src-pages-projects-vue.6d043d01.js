c698f2d52aaf945edf944365e7b185b3372039f8
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-09-28 23:05:08 +0100

    Updates

---
+++ assets/js/page--src-pages-projects-vue.6d043d01.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{fx0o:function(t,e,n){"use strict";n.r(e);var s=n("KHd+"),i=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("Layout",[e("div",[e("h2",[this._v("This is my projects page")])])])}),[],!1,null,null,null);e.default=i.exports}}]);
