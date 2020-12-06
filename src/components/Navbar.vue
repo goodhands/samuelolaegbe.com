537f644e09cada2d911754884c65630edb363260
Samuel Olaegbe <olaegbesamuel@gmail.com>
2020-12-06 19:23:50 +0100

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,9 @@
     <div>
         <nav class="nav font-body hidden sm:flex">
             <g-link class="nav__link" to="/experience/">Experience</g-link>
-            <a href="https://blog.samuelolaegbe.com">
+            <a target="_blank" href="https://blog.samuelolaegbe.com" class="nav__link flex items-center space-x-2">
                 Blog 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-3" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
                     <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
                 </svg>
